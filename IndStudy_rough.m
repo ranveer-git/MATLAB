5a0876ef8dba9d0429ba2847ea82b194faa58339
ranveer-git <ranveer-git@users.noreply.github.com>
2017-02-18 20:58:43 -0500

    Updated as on 2/18/2017

--- IndStudy_rough.m
+++ IndStudy_rough.m
@@ -11,7 +11,7 @@ s=smin:ds:smax;
 d= ((6/pi).*s).^(1/3);
 
 dt=1;
-tmax=100;
+tmax=500;
 t = 0:dt:tmax;
 N = zeros(tmax+1,smax);
 N(1,1)=10^6;
@@ -77,6 +77,7 @@ d1 = dmin:dt1:dmax;
 %D1 = repmat (d1,length(t),1);
 %S = repmat (s,length(t),1);
 transformationMatrix = zeros(2,length(d));
+N1=zeros(tmax+1,length(d1));
 for dIndex=1:length(d)
     dCurrent = d(dIndex);
     for bucketIndex = 1 : length(d1);
@@ -87,13 +88,31 @@ for dIndex=1:length(d)
        if(currentBucket <= dCurrent)
           transformationMatrix(1,dIndex) = dIndex;
           transformationMatrix(2,dIndex) = bucketIndex;
-     end
+       end
     end
+    N1(:,transformationMatrix(2,dIndex))=N1(:,transformationMatrix(2,dIndex))+N(:,transformationMatrix(1,dIndex));
+    
 end
+   sumN1=repmat((sum(N1'))',[1,8]);
+%NormMassFreq = N1./sumN1;
+figure (5)
+plot (d1,N1(6,:),...
+    d1,N1(501,:))
 
-N1=zeros(tmax+1,length(d1));
-for i = 1:tmax+1
-for j=1:length(d)
-   N1(i,transformationMatrix(2,j))=N1(i,transformationMatrix(2,j))+N(i,transformationMatrix(1,j));
+%normalised mass frequency
+NormNumFreq = N1./sumN1;
+bins = 100;
+%{
+d_new = zeros (1,length(bins));
+d_new(1,1) = floor(d(1,1));
+for i = 1:(length(bins)-1);
+    d_new(i+1)= floor (d_new(i)+0.2);
 end
-end
+%}
+   d_new = unique((floor(d./0.25)).*0.25);
+%{
+    for j=1:length(d)
+   
+    end
+  
+%}
